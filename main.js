f57dab5de2adf614b019f8625eadd899433aaa3d
Francesco <francesco.savignano96@gmail.com>
2020-11-06 14:31:56 +0100

    aggiunta comparsa e scomparsa dell'immagine tramite click del bottone

--- main.js
+++ main.js
@@ -3,11 +3,24 @@ var app = new Vue(
   {
       el: '#root',
       data: {
-          container: 'container',
+          container1: 'container1',
+          container2: 'container2',
           titleText: 'Hello VUE!!!',
           titleStyle: 'titleStyle',
-          imgByVue: 'https://italiancoders.it/wp-content/uploads/2018/01/vue.png',
-          imgStyle: 'imgStyle'
-      }
+          buttonText: 'Fai comparire l\'immagine',
+          imgByVue: 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png',
+          imgStyle: 'imgStyle1'
+      },
+      methods: {
+            changeImgDimension() {
+                if(this.imgStyle == 'imgStyle1') {
+                    // se il colore attuale è rosso => lo imposto a blu
+                    this.imgStyle = 'imgStyle2';
+                } else {
+                    // se il colore attuale è blu => lo imposto a rosso
+                    this.imgStyle = 'imgStyle1';
+                }
+            }
+        }
   }
 );
