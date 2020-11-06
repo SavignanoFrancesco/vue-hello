36a9b0ae89f54c52fb5b9c4d92919e48dd9098c1
Francesco <francesco.savignano96@gmail.com>
2020-11-06 14:15:34 +0100

    stampato titolo e immagine tramite VUE

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var app = new Vue(
+  {
+      el: '#root',
+      data: {
+          container: 'container',
+          titleText: 'Hello VUE!!!',
+          titleStyle: 'titleStyle',
+          imgByVue: 'https://italiancoders.it/wp-content/uploads/2018/01/vue.png',
+          imgStyle: 'imgStyle'
+      }
+  }
+);
